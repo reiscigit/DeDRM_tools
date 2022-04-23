747bfd006c4b9c4200e49eb63c393bdf49bede09
reiscigit <44085121+reiscigit@users.noreply.github.com>
2022-04-23 21:59:36 +0800

    Update obok.py
    Modify cmd commands for finding MAC address on Windows.

--- Obok_plugin/obok/obok.py
+++ Obok_plugin/obok/obok.py
@@ -415,13 +415,13 @@ class KoboLibrary(object):
         if sys.platform.startswith('win'):
             c = re.compile('\s?(' + '[0-9a-f]{2}[:\-]' * 5 + '[0-9a-f]{2})(\s|$)', re.IGNORECASE)
             try: 
-                output = subprocess.Popen('ipconfig /all', shell=True, stdout=subprocess.PIPE, text=True).stdout
+                output = subprocess.Popen('ipconfig /all', shell=True, stdout=subprocess.PIPE, text=True, encoding=os.device_encoding(0)).stdout
                 for line in output:
                     m = c.search(line)
                     if m:
                         macaddrs.append(re.sub("-", ":", m.group(1)).upper())
             except:
-                output = subprocess.Popen('wmic nic where PhysicalAdapter=True get MACAddress', shell=True, stdout=subprocess.PIPE, text=True).stdout
+                output = subprocess.Popen('wmic nic get MACAddress', shell=True, stdout=subprocess.PIPE, text=True).stdout
                 for line in output:
                     m = c.search(line)
                     if m:
